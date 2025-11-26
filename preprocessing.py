7ead466024196142ebc01a112b5af854d50296a4
Bokibyte <triplecreeperx@gmail.com>
2025-11-26 11:40:07 +0700

    merubah separator jadi preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -42,10 +42,32 @@ def yamlGen(cls):
     with open(f"{mainPath}/{cls}/data.yaml", "w") as f:
         yaml.dump(yamlTemplate, f, sort_keys=False)
         
+def fixLabels(cls):
+    labelsFolder = ["train", "val"]
+    
+    for folders in labelsFolder:
+        labelsFIies = glob.glob(f"{mainPath}/{cls}/labels/{folders}/*.txt")
         
+        for file in labelsFIies:
+            new_lines = []
+
+            with open(file, "r") as f:
+                for line in f:
+                    parts = line.strip().split()
+
+                    if len(parts) > 0 and parts[0] != "0":
+                        parts[0] = "0"
+
+                    new_lines.append(" ".join(parts))
+
+            with open(file, "w") as f:
+                f.write("\n".join(new_lines) + "\n")
+
+            print(f"[UPDATED] Labels with non 0 fixed {file}")
         
 for classes in weapClass:
     createClassFolder(classes)
     copyFile(classes, "train")
     copyFile(classes, "val")
+    fixLabels(classes)
     yamlGen(classes)
